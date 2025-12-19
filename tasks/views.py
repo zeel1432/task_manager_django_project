adfff5afd86e43ea658aa1c909b84897e97c9651
zeel1432 <zeelgajjar123@gmail.com>
2025-12-19 14:52:50 +0530

    added custom api

--- tasks/views.py
+++ tasks/views.py
@@ -3,6 +3,10 @@
 # Create your views here.
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework import status
 from .models import Task
 from .serializers import TaskSerializer
 
@@ -11,9 +15,39 @@ class TaskViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
+
+        queryset = Task.objects.filter(user=self.request.user)
+
         # 🔐 only logged-in user's tasks
-        return Task.objects.filter(user=self.request.user)
+        completed = self.request.query_params.get("completed")
+        priority = self.request.query_params.get("high")
+        # print(self.request.query_params)
+
+        # filter by completed
+        if completed is not None:
+            queryset = queryset.filter(
+                completed=completed.lower() == 'true'
+            )
+
+        # filter by priority
+        if priority is not None:
+            queryset = queryset.filter(priority=priority)
+
+            
+        return queryset
 
     def perform_create(self, serializer):
         # auto assign logged-in user
         serializer.save(user=self.request.user)
+
+    @action(detail=True, methods=['post'])
+    def completed(self, request, pk=None):
+        task = self.get_object()   # fetch task by ID (and user)
+
+        task.completed = True
+        task.save()
+
+        return Response(
+            {"message": "Task marked as completed"},
+            status=status.HTTP_200_OK
+    )
