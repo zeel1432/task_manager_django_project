fe1cb7b26fb5785637a0577291f7ac673de3e87d
zeel1432 <zeelgajjar123@gmail.com>
2025-12-19 13:05:54 +0530

    added features in task serializers

--- tasks/models.py
+++ tasks/models.py
@@ -4,9 +4,18 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Task(models.Model):
+    PRIORITY_CHOICES = [
+        ('low', 'Low'),
+        ('medium', 'Medium'),
+        ('high', 'High'),
+    ]
+
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     completed = models.BooleanField(default=False)
+    priority = models.CharField(max_length=10, choices=PRIORITY_CHOICES, default='medium')
+    due_date = models.DateField(null=True, blank=True)
+
 
     def __str__(self):
         return self.title
--- tasks/serializers.py
+++ tasks/serializers.py
@@ -4,4 +4,4 @@ from .models import Task
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
-        fields = ['id', 'title', 'completed']
+        fields = ['id', 'title', 'completed', 'priority', 'due_date']
